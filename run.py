f08ff9cfdc116456eae66e49c9d961048bc63238
SerjMartin <serjmartin@mail.com>
2021-10-21 17:18:24 +0000

    Add coments to run.py file

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
-import os
+import os # import os from python library
 from flask import Flask
-
+# from flask import class name Flask
+# capital latter mean class name
 
 app = Flask(__name__)
+# variable call app
 
 
-@app.route("/")
-def index():
+@app.route("/")  # app.outer decorator @(pie-notation)
+# "/"  triggers the index function
+def index():  # this function return only Hello, World
     return "Hello, World"
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # __name__ is he default name in python
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
