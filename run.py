9c1be3fc08d1542b55dc84edc00f1a06a56fd77a
SerjMartin <serjmartin@mail.com>
2021-10-22 16:37:06 +0000

    Add contact.html file and core structure to it

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os # import os from python library
+import os  # import os from python library
 from flask import Flask, render_template
 # from flask import class name Flask
 # capital latter mean class name
@@ -10,7 +10,17 @@ app = Flask(__name__)
 @app.route("/")  # app.outer decorator @(pie-notation)
 # "/"  triggers the index function
 def index():  # this function return only Hello, World
-    return  render_template("index.html")  # "<h1>Hello,</h1> <h2>World</h2>"
+    return render_template("index.html")  # "<h1>Hello,</h1> <h2>World</h2>"
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":  # __name__ is he default name in python
