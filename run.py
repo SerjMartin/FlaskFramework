7afa64c7c7ed782923d912e7a3a53f630522c1c0
SerjMartin <serjmartin@mail.com>
2021-10-26 20:14:14 +0000

    Add data folder

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os  # import os from python library
+import json
 from flask import Flask, render_template
 # from flask import class name Flask
 # capital latter mean class name
@@ -15,17 +16,20 @@ def index():  # this function return only Hello, World
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":  # __name__ is he default name in python
